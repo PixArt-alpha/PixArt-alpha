300e173758fab129af55c10b4813b0199d554a0a
chenjunsong <cjs1020440147@icloud.com>
2023-10-28 11:56:13 +0800

    Del T2ICompBench code for simplicity.

--- eval_t2icompbench/UniDet_eval/test.sh
+++
@@ -1,8 +0,0 @@
-project_dir=UniDet_eval
-
-folder=$1
-ability=$2
-cuda=$3
-out_dir=../../output/T2ICompBench/junsong_organized_generation/$folder/$ability
-
-cd $project_dir && CUDA_VISIBLE_DEVICES=$cuda python determine_position_for_eval.py --outpath $out_dir > tmp.out
