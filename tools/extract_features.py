fa91a959e911e9ff965918c140b948e1addeaec6
kopyl <oleggggg1997@gmail.com>
2023-12-01 11:38:25 +0200

    Remove commented code

--- tools/extract_features.py
+++ tools/extract_features.py
@@ -32,8 +32,6 @@ def extract_caption_t5():
         for item in tqdm(train_data):
 
             caption = item['prompt'].strip()
-            # if caption in captions:
-            #     continue
             captions.add(caption)
             if isinstance(caption, str):
                 caption = [caption]
