1b77db533db5c96244ba7f3739925227b092808b
jschen <chenjunsong4@h-partners.com>
2023-12-28 23:29:55 +0800

    tmp code

--- train.sh
+++ train.sh
@@ -1,19 +1,3 @@
-# CUDA_VISIBLE_DEVICES=5,6,7 python -m torch.distributed.launch --nproc_per_node=3 \
-#     --master_port=26662 train_scripts/train_controlnet.py \
-#     ./configs/pixart_app_config/PixArt_xl2_img512_controlHed.py \
-#     --work-dir output_debug \
-#     --controlnet_type half
-    # --resume_from '/home/xieenze/efs_cv/yue/controlnet_all_0.5data_drop0.3_lr2e-5/checkpoints/epoch_7_step_22000.pth' \
-
-# CUDA_VISIBLE_DEVICES=5,6,7 python -m torch.distributed.launch --nproc_per_node=3 \
-#     --master_port=26662 train_scripts/train_controlnet.py \
-#     ./configs/pixart_app_config/PixArt_xl2_img1024_controlHed_drop.py \
-#     --work-dir output_debug \
-#     --controlnet_type all \
-#     --resume_from '/home/xieenze/efs_cv/yue/controlnet_all_0.5data_drop0.3_lr2e-5/checkpoints/epoch_7_step_22000.pth' \
-#     --resume_optimizer \
-#     --resume_lr_scheduler
-
 CUDA_VISIBLE_DEVICES=5,6,7 python -m torch.distributed.launch --nproc_per_node=3 \
     --master_port=26662 train_scripts/train_controlnet.py \
     ./configs/pixart_app_config/PixArt_xl2_img1024_controlHed_Half.py \
