894de4c455b61108eabb2f6e59f8b20351235b7f
jschen <chenjunsong4@h-partners.com>
2024-01-17 21:42:39 +0800

    code update

--- diffusion/data/datasets/pixart_control.py
+++ diffusion/data/datasets/pixart_control.py
@@ -59,15 +59,14 @@ class InternalDataHed(Dataset):
         total_sample = len(self.img_samples)
         print(total_sample, self.ori_imgs_nums)
         used_sample_num = int(total_sample * train_ratio)
+        print("using mode", mode)
         if mode == 'train':
-            print("using mode", mode)
             self.img_samples = self.img_samples[:used_sample_num]
             self.txt_feat_samples = self.txt_feat_samples[:used_sample_num]
             self.vae_feat_samples = self.vae_feat_samples[:used_sample_num]
             self.hed_feat_samples = self.hed_feat_samples[:used_sample_num]
             self.prompt_samples = self.prompt_samples[:used_sample_num]
         else:
-            print("using mode", mode)
             self.img_samples = self.img_samples[-used_sample_num:]
             self.txt_feat_samples = self.txt_feat_samples[-used_sample_num:]
             self.vae_feat_samples = self.vae_feat_samples[-used_sample_num:]
