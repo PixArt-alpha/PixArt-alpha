9c8821b18c265cf8ff0106a709695471e232ac03
raci0399 <raci0399@gmail.com>
2024-06-25 18:21:39 +0200

    tracker name

--- controlnet/train_pixart_controlnet_hf.py
+++ controlnet/train_pixart_controlnet_hf.py
@@ -659,7 +659,7 @@ def main():
     # We need to initialize the trackers we use, and also store our configuration.
     # The trackers initializes automatically on the main process.
     if accelerator.is_main_process:
-        accelerator.init_trackers("text2image-fine-tune", config=vars(args))
+        accelerator.init_trackers("pixart-fine-tune", config=vars(args))
 
     # Train!
     total_batch_size = args.train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
