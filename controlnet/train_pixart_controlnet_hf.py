68a1fbc78b28fa97aa8e47d3e4403d81b7b56ea4
raci0399 <raci0399@gmail.com>
2024-06-27 19:30:27 +0200

    remove function call, is defined for peft models only

--- controlnet/train_pixart_controlnet_hf.py
+++ controlnet/train_pixart_controlnet_hf.py
@@ -444,8 +444,6 @@ def main():
 
     transformer.train()    
 
-    transformer.print_trainable_parameters()
-
     def unwrap_model(model):
         model = accelerator.unwrap_model(model)
         model = model._orig_mod if is_compiled_module(model) else model
