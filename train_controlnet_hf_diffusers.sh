9033b27022056b7e6cecbd2e2739f70a8860a5f5
raci0399 <raci0399@gmail.com>
2024-07-02 09:39:45 +0200

    train controlnet bash script

---
+++ train_controlnet_hf_diffusers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# run
+# accelerate config
+
+# check with
+# accelerate env
+
+export MODEL_DIR="PixArt-alpha/PixArt-XL-2-1024-MS"
+export OUTPUT_DIR="pixart-controlnet-hf-diffusers-test"
+
+accelerate launch ./controlnet/train_pixart_controlnet_hf.py \
+ --pretrained_model_name_or_path=$MODEL_DIR \
+ --output_dir=$OUTPUT_DIR \
+ --dataset_name=fusing/fill50k \
+ --mixed_precision="fp16" \
+ --resolution=1024 \
+ --learning_rate=1e-5 \
+ --max_train_steps=15000 \
+ --validation_image "./conditioning_image_1.png" "./conditioning_image_2.png" \
+ --validation_prompt "red circle with blue background" "cyan circle with brown floral background" \
+ --validation_steps=100 \
+ --train_batch_size=1 \
+ --gradient_accumulation_steps=4 \
+ --report_to="wandb" \
+ --seed=42 \
+ --dataloader_num_workers=8
+#  --lr_scheduler="cosine" --lr_warmup_steps=0 \
